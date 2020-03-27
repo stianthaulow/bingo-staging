ad05191345dedd63eb0182fa1fad910f57e9ec86
Stian Thaulow <stian@thaulow.me>
2020-03-27 14:46:56 +0100

    Updates

---
+++ precache-manifest.f5d4560ad6c21049d1dda401c0a50585.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da18d03f247a902f3db2ba8824351b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46cd7755749987700cf2",
+    "url": "/static/css/main.0df97595.chunk.css"
+  },
+  {
+    "revision": "11a91566a1427a29694e",
+    "url": "/static/js/2.ac164ab7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ac164ab7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46cd7755749987700cf2",
+    "url": "/static/js/main.e6465fb2.chunk.js"
+  },
+  {
+    "revision": "5018ba84458e34cd06e6",
+    "url": "/static/js/runtime-main.4ec088e6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bingo-staging/precache-manifest.6c2433aa9e8899a28c4d3ed620bd31f9.js"
+  "/precache-manifest.f5d4560ad6c21049d1dda401c0a50585.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bingo-staging/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
